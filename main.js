d95b8a46a545cae61e4980bbf2ee65c79aaab686
Pooja Shroff <poshroff@paypalcorp.com>
2017-11-07 21:54:54 -0800

    ES6

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var circle_1 = require("./math/circle"); // import * as Circle from './math/circle';
-var rectangle_1 = require("./math/rectangle");
+const circle_1 = require("./math/circle"); // import * as Circle from './math/circle';
+const rectangle_1 = require("./math/rectangle");
 require("jQuery");
 console.log("Hi there");
 var burger = "hamburger", calories = 300, tasty = true; //Change this to a string and see what happens
@@ -28,37 +18,34 @@ var ice_cream = {
 };
 speakObj(ice_cream);
 /*Classes*/
-var Menu = /** @class */ (function () {
-    function Menu(item_list, total_pages) {
+class Menu {
+    constructor(item_list, total_pages) {
         this.items = item_list;
         this.pages = total_pages;
     }
-    Menu.prototype.list = function () {
+    list() {
         console.log("Our menu for today:");
         for (var index = 0; index < this.items.length; index++) {
             console.log(this.items[index]);
         }
-    };
-    return Menu;
-}());
+    }
+}
 var sundayMenu = new Menu(["pancakes", "waffles", "orange juice"], 1);
 sundayMenu.list();
-var HappyMeal = /** @class */ (function (_super) {
-    __extends(HappyMeal, _super);
+class HappyMeal extends Menu {
     //Inheritance
-    function HappyMeal(item_list, total_pages) {
-        return _super.call(this, item_list, total_pages) || this;
+    constructor(item_list, total_pages) {
+        super(item_list, total_pages);
     }
     //Overriding parent class function
-    HappyMeal.prototype.list = function () {
+    list() {
         console.log("Our special menu for children:");
         for (var index = 0; index < this.items.length; index++) {
             var element = this.items[index];
             console.log(element);
         }
-    };
-    return HappyMeal;
-}(Menu));
+    }
+}
 var happyMealMenu = new HappyMeal(["candy", "drink", "toy"], 1);
 happyMealMenu.list();
 /*Generics*/
@@ -67,8 +54,8 @@ function genericFunc(argument) {
     arrayOfT.push(argument);
     return arrayOfT;
 }
-console.log('Generics');
-var arrayFromString = genericFunc("beep"); //Best practice to set type 
+console.log("Generics");
+var arrayFromString = genericFunc("beep"); //Best practice to set type
 console.log(arrayFromString[0]);
 console.log(typeof arrayFromString[0]);
 var arrayFromNumber = genericFunc(42); //Inferred type
@@ -79,3 +66,78 @@ console.log(circle_1.PI); //console.log(Circle.PI);
 console.log(circle_1.calcCircumference(10));
 console.log(rectangle_1.default(10, 20));
 $("#app-id").css({ "background-color": "lightblue" });
+//enums
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let myColor = Color.Green;
+console.log(myColor);
+let bankAccount = {
+    money: 2000,
+    deposit(value) {
+        this.money += value;
+    }
+};
+let myself = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
+/*es6*/
+let variable = "Test"; //Mutable
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+const maxLevels = 100; //Immutable
+console.log(maxLevels);
+//Block scope
+function reset() {
+    let variable = null;
+    console.log(variable); //prints null
+}
+reset();
+console.log(variable); //prints "Another value"
+console.log("Arrow functions");
+function addNumbers(number1, number2) {
+    return number1 + number2;
+}
+console.log(addNumbers(2, 3));
+const multiplyNumbers = (number1, number2) => number1 * number2;
+console.log(multiplyNumbers(5, 6));
+const greet = () => {
+    console.log("Hi");
+};
+greet();
+const greetFriend = (friend) => console.log("Hi", friend);
+greetFriend("Pooja");
+//Default parameters
+const countdown = (start = 10) => {
+    while (start > 0) {
+        start--;
+    }
+    console.log("Done", start);
+};
+countdown();
+//Spread
+const numbers = [2, 3, 10, -1];
+console.log(Math.max(...numbers)); //Basically all array elements are spread to individual parameters
+function makeArray(name, ...args) {
+    return args;
+}
+console.log(makeArray("Max", 2, 4, 6));
+console.log("Destructuring");
+const hobbies = ["Cooking", "Sports"];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+const userData = { username: "Pooja", age: 27 };
+const { username: myName, age: myAge } = userData;
+console.log(myName, myAge);
+//Template Literals
+const username = "Pooja";
+const greeting = `Hi I am ${username} I am so cool!`;
+console.log(greeting);
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-import { PI, calcCircumference} from './math/circle'; // import * as Circle from './math/circle';
-import calc from "./math/rectangle"; 
+import { PI, calcCircumference } from "./math/circle"; // import * as Circle from './math/circle';
+import calc from "./math/rectangle";
 import "jQuery";
 
 console.log("Hi there");
@@ -30,7 +30,6 @@ var ice_cream = {
 };
 
 speakObj(ice_cream);
-
 
 /*Classes*/
 
@@ -77,24 +76,125 @@ var happyMealMenu = new HappyMeal(["candy", "drink", "toy"], 1);
 happyMealMenu.list();
 
 /*Generics*/
-function genericFunc<T>(argument: T): T[]{
-    var arrayOfT: T[] = [];
-    arrayOfT.push(argument);
-    return arrayOfT;
+function genericFunc<T>(argument: T): T[] {
+  var arrayOfT: T[] = [];
+  arrayOfT.push(argument);
+  return arrayOfT;
 }
-console.log('Generics');
-var arrayFromString = genericFunc<string>("beep"); //Best practice to set type 
+console.log("Generics");
+var arrayFromString = genericFunc<string>("beep"); //Best practice to set type
 console.log(arrayFromString[0]);
 console.log(typeof arrayFromString[0]);
 
-var arrayFromNumber = genericFunc(42);  //Inferred type
+var arrayFromNumber = genericFunc(42); //Inferred type
 console.log(arrayFromNumber[0]);
 console.log(typeof arrayFromNumber[0]);
-
 
 /*Modules*/
 console.log(PI); //console.log(Circle.PI);
 console.log(calcCircumference(10));
 console.log(calc(10, 20));
 
-$("#app-id").css({"background-color": "lightblue"});
+$("#app-id").css({ "background-color": "lightblue" });
+
+//enums
+
+enum Color {
+  Gray,
+  Green,
+  Blue
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+type BankAccount = { money: number; deposit: (val: number) => void };
+let bankAccount: BankAccount = {
+  money: 2000,
+  deposit(value: number): void {
+    this.money += value;
+  }
+};
+
+let myself: { name: string; bankAccount: BankAccount; hobbies: string[] } = {
+  name: "Max",
+  bankAccount: bankAccount,
+  hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
+
+/*es6*/
+let variable = "Test"; //Mutable
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+
+const maxLevels = 100; //Immutable
+console.log(maxLevels);
+
+//Block scope
+function reset() {
+  let variable = null;
+  console.log(variable); //prints null
+}
+reset();
+console.log(variable); //prints "Another value"
+
+console.log("Arrow functions");
+
+function addNumbers(number1: number, number2: number) {
+  return number1 + number2;
+}
+console.log(addNumbers(2, 3));
+
+const multiplyNumbers = (number1: number, number2: number) => number1 * number2;
+
+console.log(multiplyNumbers(5, 6));
+
+const greet = () => {
+  console.log("Hi");
+};
+greet();
+
+const greetFriend = (friend: string) => console.log("Hi", friend);
+
+greetFriend("Pooja");
+
+//Default parameters
+const countdown = (start: number = 10): void => {
+  while (start > 0) {
+    start--;
+  }
+  console.log("Done", start);
+};
+
+countdown();
+
+//Spread
+
+const numbers = [2, 3, 10, -1];
+console.log(Math.max(...numbers)); //Basically all array elements are spread to individual parameters
+
+function makeArray(name: string, ...args: number[]) { //unknown number of params.
+  return args;
+}
+
+console.log(makeArray("Max", 2,4,6));
+
+console.log("Destructuring");
+const hobbies = ["Cooking", "Sports"];
+const[ hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2)
+
+const userData = {username: "Pooja", age: 27};
+const {username: myName, age:myAge} =  userData;
+console.log(myName, myAge);
+
+//Template Literals
+
+const username = "Pooja";
+const greeting = `Hi I am ${username} I am so cool!`;
+console.log(greeting);
